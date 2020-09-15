8024790d7a8d226fde78ed2975854290ebf9b51d
BK <brian_j_king@me.com>
2020-09-14 21:49:04 -0500

    Add Class WordSmith,Test, and Test Passed

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("noon", true)]
+        [InlineData("car", false)]
+        public void Test(string word,bool expected)
         {
-
+            //arrange
+            var wordSmith = new WordSmith();
+            //act
+            var actual = wordSmith.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
